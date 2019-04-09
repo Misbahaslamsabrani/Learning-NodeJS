8e5ac76e6271f3e5132d6700201eb45301107000
misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2019-04-09 05:47:38 +0500

    Lesson1

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -7,8 +7,8 @@ const myEmail = "misbah@gmail.com";
 console.log(validator.isEmail(myEmail)); */
 
 
-/* const chalk = require('chalk');
-console.log(chalk.bgMagenta.bold.white('Hello world!')); 
+const chalk = require('chalk');
+/* console.log(chalk.bgMagenta.bold.white('Hello world!')); 
 const myName = "misbah";
 console.log(chalk`
 my name is {bold.red ${myName}} !`) */
@@ -22,20 +22,47 @@ const yargs = require("yargs")
 /* console.log(yargs.argv)
 console.log(yargs.argv.username) */
 
-yargs.command({
-    command: "add",
-    describe: "Adding Todo in the list",
-    builder: {
-        title: "add todo",
-        alias: "a",
-        demandOption: true,
-        type: "string",
-        //default: "...",
-    },
-    handler(argv){
-        console.log(argv.title)
-
+/* yargs.command({
+command: "add",
+describe: "Adding Todo in the list",
+builder: {
+    addtodo: {
+    describe: "add to todo",
+    alias: "a",
+    demandOption: true,
+    type: "string",
+    //default: "...",
     }
+},
+handler(argv){
+    console.log(argv)
+    console.log("okay done!",argv.add)
+
+}
 })
-yargs.parse();
-console.log("server running....")
+yargs.parse(); */
+console.log(chalk.bold.red("Server Running...."))
+
+//https://github.com/yargs/yargs/blob/HEAD/docs/advanced.md#commands
+//http://yargs.js.org/docs/
+
+const fs = require("fs");
+
+const obj = {name: "misbah", gender: "female"}
+const convOBj = JSON.stringify(obj);
+console.log(convOBj)
+
+fs.writeFileSync("DATA.text", convOBj) 
+
+// fs.appendFileSync("DATA.text", convOBj)
+
+
+try {
+    const data = fs.readFileSync("DATA.text");
+    //console.log(data)
+    const parData = JSON.parse(data)
+    console.log(parData)
+}
+catch (e) {
+    console.log(e.message)
+}
